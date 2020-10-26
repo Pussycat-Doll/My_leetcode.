66c4a34d33767981a783269312f862962ffde771
Pussycat-Doll <2633974012@qq.com>
2020-10-26 08:08:09 +0800

    2020/10/26

--- 求1+2+3+...+n.cpp
+++ 求1+2+3+...+n.cpp
@@ -1,3 +1,27 @@
+class Sum
+{
+public:
+    Sum()
+    {
+        _sum += _a;
+        ++_a;
+    }
+    static int print()
+    {
+        return _sum;
+    }
+    static void Init()
+    {
+        _a = 1;
+        _sum = 0;
+    }
+private:
+    static int _a;
+    static int _sum;
+};
+int Sum::_a = 1;
+int Sum::_sum = 0;
+
 class Solution {
 public:
     int Sum_Solution(int n) {
@@ -5,6 +29,9 @@ public:
 //             return 1;
 //         return n +  Sum_Solution(n-1);
         
-        return n*(n+1)/2;
+//        return n*(n+1)/2;
+        Sum::Init();
+       Sum a[n];
+       return Sum::print();
     }
 };
