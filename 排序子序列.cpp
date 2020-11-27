e04c4e0f44f0d27a86a50cc726b45214075b5b4d
Pussycat-Doll <2633974012@qq.com>
2020-11-27 23:46:22 +0800

    2020/11/27

--- 排序子序列.cpp
+++ 排序子序列.cpp
@@ -1,51 +1,96 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    cin>>n;
+//    vector<int> arr;
+//    while(n--)
+//    {
+//        int x;
+//        cin>>x;
+//        arr.push_back(x);
+//    }
+//    
+//    int r = 0;
+//    int flag = 0;
+//    int i = 1;
+//    while(i < arr.size())
+//    {
+//        while(i < arr.size() && arr[i - 1] == arr[i])
+//        {
+//            ++i;
+//        }
+//        while(i < arr.size() && arr[i-1] < arr[i])
+//        {
+//            ++i;
+//            flag = 1;
+//        }
+//        if(flag == 1)
+//        {
+//            ++i;
+//            ++r;
+//            flag = 0;
+//        }
+//        while(i < arr.size() && arr[i-1] > arr[i])
+//        {
+//            ++i;
+//            flag = 1;
+//        }
+//        if(flag == 1)
+//        {
+//            ++i;
+//            ++r;
+//            flag = 0;
+//        }
+//    }
+//    cout<<r<<endl;
+//    return 0;
+//}
 #include<iostream>
 #include<vector>
 using namespace std;
-
 int main()
 {
     int n;
-    cin>>n;
-    vector<int> arr;
-    while(n--)
+    while(cin>>n)
     {
-        int x;
-        cin>>x;
-        arr.push_back(x);
+        vector<int> arr;
+        arr.resize(n+1);
+        for(int i = 0; i < n; ++i)
+        {
+            cin>>arr[i];
+        }
+        arr[n]=0;
+        int i = 0;
+        int count = 0;
+        while(i < n)
+        {
+            if(arr[i] > arr[i+1])//进入非递减序列 
+            {
+                while(i < n && arr[i] >= arr[i+1])
+                {
+                    i++;
+                }
+                count++;
+                i++;
+            }
+            else if(arr[i] == arr[i + 1])//跳过相等的元素 
+                i++;
+            else//进入非递增序列 
+            {
+                while(i < n && arr[i] <= arr[i+1])
+                {
+                    i++;
+                }
+                count++;
+                i++;
+            }
+        }
+        cout<<count<<endl;
     }
-    
-    int r = 0;
-    int flag = 0;
-    int i = 1;
-    while(i < arr.size())
-    {
-        while(i < arr.size() && arr[i - 1] == arr[i])
-        {
-            ++i;
-        }
-        while(i < arr.size() && arr[i-1] < arr[i])
-        {
-            ++i;
-            flag = 1;
-        }
-        if(flag == 1)
-        {
-            ++i;
-            ++r;
-            flag = 0;
-        }
-        while(i < arr.size() && arr[i-1] > arr[i])
-        {
-            ++i;
-            flag = 1;
-        }
-        if(flag == 1)
-        {
-            ++i;
-            ++r;
-            flag = 0;
-        }
-    }
-    cout<<r<<endl;
+        
     return 0;
 }
