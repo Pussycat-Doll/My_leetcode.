a8c9dee179daa669764576f2d1df5f0ba6fc1048
Pussycat-Doll <2633974012@qq.com>
2020-11-27 23:47:18 +0800

    2020/11/27

--- 字符串中找出最长数字串.cpp
+++ 字符串中找出最长数字串.cpp
@@ -1,36 +1,61 @@
+//#include<iostream>
+//#include<string>
+//#include<queue>
+//using namespace std;
+//
+//int main()
+//{
+//    string s;
+//    cin>>s;
+//    string temp;
+//    queue<string>  res;
+//    for(size_t i = 0; i < s.size(); ++i)
+//    {
+//        while(s[i] >= '0' && s[i] <= '9')
+//        {
+//            temp+=s[i];
+//            ++i;
+//        }
+//        if(!temp.empty())
+//        {
+//            res.push(temp);
+//            temp = "";
+//        }
+//    }
+//    string _max = res.front();
+//    while(!res.empty())
+//    {
+//        if(res.front().size() > _max.size())
+//        {
+//            _max = res.front();
+//        }
+//        res.pop();
+//    }
+//    cout<<_max;
+//    return 0;
+//}
+
 #include<iostream>
 #include<string>
-#include<queue>
 using namespace std;
-
 int main()
 {
     string s;
     cin>>s;
-    string temp;
-    queue<string>  res;
-    for(size_t i = 0; i < s.size(); ++i)
+    
+    string res,cur;
+    res = "";
+    for(int i = 0; i <= s.size(); ++i)
     {
-        while(s[i] >= '0' && s[i] <= '9')
+        if(s[i] >= '0' && s[i] <= '9')
+            cur+=s[i];
+        else
         {
-            temp+=s[i];
-            ++i;
-        }
-        if(!temp.empty())
-        {
-            res.push(temp);
-            temp = "";
+            if(cur.size() > res.size())
+               res = cur;
+            cur = "";
         }
     }
-    string _max = res.front();
-    while(!res.empty())
-    {
-        if(res.front().size() > _max.size())
-        {
-            _max = res.front();
-        }
-        res.pop();
-    }
-    cout<<_max;
+    cout<<res<<endl;
     return 0;
 }
