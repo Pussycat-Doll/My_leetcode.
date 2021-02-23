ad2193efb65c55e9b224fcd68af1c37bcc0f5e6f
Pussycat-Doll <2633974012@qq.com>
2021-02-23 23:40:30 +0800

    2021/02/23

---
+++ 两个链表的公共结点.cpp
@@ -0,0 +1,22 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) 
+    {
+        ListNode* p1 = pHead1;
+        ListNode* p2 = pHead2;
+        while(p1 != p2)
+        {
+            p1 = p1!=nullptr?p1->next:pHead2;
+            p2 = p2!=nullptr?p2->next:pHead1;
+        }
+        return p1;
+    }
+};
